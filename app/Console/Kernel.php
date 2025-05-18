af15c75c16ccccc5b4e4133de62e64278e072b1b
AMK1215 <aungmyokhaing1215@gmail.com>
2025-05-18 10:25:24 +0630

    update laravel api call

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
         //Commands\PullReport::class,
         // Commands\ArchiveOldWagers::class,
         // Commands\DeleteOldWagerBackups::class,
-        Commands\TriggerGameSpin::class
+        //Commands\TriggerGameSpin::class
     ];
 
     /**
